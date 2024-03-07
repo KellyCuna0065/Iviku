655abb58ef562748a2336bd11eeb844b3b6b6c64
Kelly Cuna <ti20110065@itess.edu.mx>
2024-03-06 21:59:35 -0600

    MiniPaint: proceso drawLine actualizado y mejorado

--- app/src/main/java/com/example/iviku/MiniPaint.kt
+++ app/src/main/java/com/example/iviku/MiniPaint.kt
@@ -1,14 +1,5 @@
 package com.example.iviku
 
-/*import android.graphics.*
-
-interface MiniPaint {
-    var paint: Paint
-    fun onButtonClick()
-    fun onTouchEvent()
-    fun clearCanvas()
-}*/
-
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
@@ -18,9 +9,7 @@ import android.view.View
 class MiniPaint (context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
 
     private var paint = Paint()
-    private var startPoint: PointF? = null
-    private var endPoint: PointF? = null
-    private var isDrawing: Boolean = false
+    private var lines = mutableListOf<Pair<PointF, PointF>>()
     var option: Int = 0
 
     init {
@@ -32,8 +21,8 @@ class MiniPaint (context: Context, attrs: AttributeSet? = null) : View(context, 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        if (isDrawing) {
-            canvas.drawLine(startPoint!!.x, startPoint!!.y, endPoint!!.x, endPoint!!.y, paint)
+        for (line in lines) {
+            canvas.drawLine(line.first.x, line.first.y, line.second.x, line.second.y, paint)
         }
     }
 
@@ -41,23 +30,15 @@ class MiniPaint (context: Context, attrs: AttributeSet? = null) : View(context, 
 
         when (option) {
             1 -> {
-                paint.color = Color.BLUE
                 when (event!!.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        startPoint = PointF(event.x, event.y)
-                        endPoint = PointF()
-                        isDrawing = true
+                        val startPoint = PointF(event.x, event.y)
+                        lines.add(Pair(startPoint, PointF()))
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        endPoint!!.x = event.x
-                        endPoint!!.y = event.y
+                        val lastIndex = lines.lastIndex
+                        lines[lastIndex] = Pair(lines[lastIndex].first, PointF(event.x, event.y))
                         invalidate()
-                    }
-                    MotionEvent.ACTION_UP -> {
-
-                    }
-                    else -> {
-                        isDrawing = false
                     }
                 }
 
@@ -80,7 +61,7 @@ class MiniPaint (context: Context, attrs: AttributeSet? = null) : View(context, 
     }
 
     fun clearCanvas() {
-        paint.color = Color.WHITE
+        lines.clear()
         invalidate()
     }
 
